b104420892e1540c0a173809131739d7326fcb64
Alistair Martin <alistairmartin01@gmail.com>
2017-08-03 19:54:38 +0930

    hw6: Test read vs fread, and 0 vs 1 byte

--- chapter_06/measure_syscall.c
+++ chapter_06/measure_syscall.c
@@ -1,29 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #define SAMPLES 1000
 #define BASE_10 10
 
 void time_array(struct timeval times[SAMPLES]);
-double average_syscall_cost(int samples);
-double average_syscall_cost_2(int samples);
+double avg_syscall_cost(int samples, size_t nbyte);
+double avg_syscall_cost_fopen(int samples, size_t nbyte);
+double avg_syscall_cost_2(int samples);
 
 int main(int argc, char *argv[])
 {
-    double avg_syscall; 
-    long usr_samples;
-
     /* if the user provides an integer use that as the # of samples */
-    usr_samples = argc > 1 ? strtol(argv[1], NULL, BASE_10) : 0;
+    long usr_samples = argc > 1 ? strtol(argv[1], NULL, BASE_10) : 0;
     usr_samples = (usr_samples ? usr_samples : SAMPLES);
 
     /* perform experiments */
-    avg_syscall = average_syscall_cost(usr_samples);
-    printf("The average time for a system call is %.3fms\n", avg_syscall);
-    
-    avg_syscall = average_syscall_cost_2(usr_samples);
-    printf("The average time for a system call is %.3fms\n", avg_syscall);
+    printf("The average time for a system call (read, 0-byte) is %.3fms\n", 
+            avg_syscall_cost(usr_samples, 0));
+    printf("The average time for a system call (read, 1-byte) is %.3fms\n", 
+            avg_syscall_cost(usr_samples, 1));
+    printf("The average time for a system call (fread, 0-byte) is %.3fms\n",
+            avg_syscall_cost_fopen(usr_samples, 0));
+    printf("The average time for a system call (fread, 1-byte) is %.3fms\n",
+            avg_syscall_cost_fopen(usr_samples, 1));
+    printf("The average time for a system call is %.3fms\n",
+            avg_syscall_cost_2(usr_samples));
     return 0;
 }
 
@@ -37,7 +42,47 @@ int main(int argc, char *argv[])
  * @param samples, the number of measurements taken
  * @return the average time to perform the syscall in microseconds.  
  */
-double average_syscall_cost(int samples)
+double avg_syscall_cost(int samples, size_t nbyte)
+{
+    struct timeval start, end;
+    suseconds_t diff;
+    unsigned long ms_total = 0;
+    int tod1, tod2, file;
+    char buf;
+
+    for (int i=0; i < samples; i++) {
+        /* open the file */
+        file = open("blank_file.output", O_RDONLY|O_CREAT);
+        if (file == -1) {
+            fprintf(stderr, "file error\n");
+            exit(EXIT_FAILURE);
+        }
+
+        /* benchmark */
+        tod1 = gettimeofday(&start, NULL);  // start time
+        read(file, &buf, nbyte);
+        tod2 = gettimeofday(&end, NULL);    // end time
+        
+        if (tod1 == -1 || tod2 == -1) {
+            fprintf(stderr, "failed benchmarking time\n");
+            exit(EXIT_FAILURE);
+        }
+
+        diff = end.tv_usec - start.tv_usec;
+        ms_total += (int) diff;
+
+        /* close the file */
+        close(file);
+    }
+    return (double) ms_total / (double) samples;
+}
+
+/*
+ * Measure the costs of a system call, by repeatedly performing a 0-byte read.
+ * @param samples, the number of measurements taken
+ * @return the average time to perform the syscall in microseconds.  
+ */
+double avg_syscall_cost_fopen(int samples, size_t nbyte)
 {
     struct timeval start, end;
     char c;
@@ -56,7 +101,7 @@ double average_syscall_cost(int samples)
 
         /* benchmark */
         tod1 = gettimeofday(&start, NULL);  // start time
-        fread(&c, 1, 1, file);              // perform syscall
+        fread(&c, 1, nbyte, file);              // perform syscall
         tod2 = gettimeofday(&end, NULL);    // end time
         
         if (tod1 == -1 || tod2 == -1) {
@@ -70,7 +115,6 @@ double average_syscall_cost(int samples)
         /* close the file */
         fclose(file);
     }
-
     return (double) ms_total / (double) samples;
 }
 
@@ -79,7 +123,7 @@ double average_syscall_cost(int samples)
  * @param samples, the number of measurements taken
  * @return the average time to perform the syscall in microseconds.  
  */
-double average_syscall_cost_2(int samples)
+double avg_syscall_cost_2(int samples)
 {
     struct timeval start, end;
     char c;
@@ -117,8 +161,9 @@ double average_syscall_cost_2(int samples)
 
 /*
  * QUESTIONS:
- * 1. presumably the fopen() and fread() procedures are equivalent to the open() and read() syscalls?
- * 2. There's a significant difference between average_syscall_cost() and average_syscall_cost_2(). Is that due to caching?
+ * 1. a) Do the fopen(), fread(), etc, procedures call open(), read() (etc) or implement their own syscalls?
+ *    b) Presumably the former exist as syntactic sugar, and as a cross OS implementation?
+ *    c) Why such a discrepency in between read() and fread()?
  * 3. Is there an OSX equivalent to Linux's sched_setaffinity()?
  * 4. If not, will the above procedure work in a VM? 
  */
