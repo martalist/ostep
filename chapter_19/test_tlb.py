485af98eb349c5202f46d39d7ad06963cb402d80
Alistair Martin <alistairmartin01@gmail.com>
2017-08-29 16:38:27 +0930

    Fix bug in tlb.c that caused incorrect benchmarking

--- chapter_19/test_tlb.py
+++ chapter_19/test_tlb.py
@@ -4,5 +4,5 @@ import subprocess
 
 if __name__ == "__main__":
     print("Pages\tTrials\tMicroSeconds")
-    for i in range(1, 100, 2):
-        subprocess.call(["./tlb.out", "{}".format(i), "10000"])
+    for i in range(1, 2050, 2):
+        subprocess.call(["./tlb.out", str(i), "10000"])
--- chapter_19/tlb.c
+++ chapter_19/tlb.c
@@ -13,6 +13,7 @@
 
 #define SAMPLES 0x09000000
 #define BASE_10 10
+#define MS_PER_SEC 1000000
 
 double run_trials(int page_size, int num_pages, int trials);
 
@@ -63,7 +64,7 @@ int main(int argc, char *argv[])
 double run_trials(int page_size, int num_pages, int trials)
 {
     struct timeval start, end;
-    uint64_t ms_total = 0;
+    uint64_t trial_time = 0;
     int tod1, tod2, elems, jump, i, j;
     int *arr, arr_size;
 
@@ -75,19 +76,18 @@ double run_trials(int page_size, int num_pages, int trials)
     jump = page_size / sizeof(int);
 
     /* run trials */
+    tod1 = gettimeofday(&start, NULL);  // start time
     for (j = 0; j < trials; j++) {
         for (i = 0; i < num_pages * jump; i += jump) {
-            tod1 = gettimeofday(&start, NULL);  // start time
             arr[i] += 1;                        // page access
-            tod2 = gettimeofday(&end, NULL);    // end time
-
-            if (tod1 == -1 || tod2 == -1) {
-                fprintf(stderr, "failed benchmarking time\n");
-                exit(EXIT_FAILURE);
-            }
-            ms_total += end.tv_usec - start.tv_usec;
         }
     }
+    tod2 = gettimeofday(&end, NULL);    // end time
+    if (tod1 == -1 || tod2 == -1) {
+        fprintf(stderr, "failed benchmarking time\n");
+        exit(EXIT_FAILURE);
+    }
+    trial_time = (end.tv_sec - start.tv_sec) * MS_PER_SEC + end.tv_usec - start.tv_usec;
     free(arr);
-    return ms_total / (double) (num_pages * trials);
+    return trial_time / (double) (num_pages * trials);
 }
