611c7296852f9d928f90cf85a73e61f6ffb3ce46
Alistair Martin <alistairmartin01@gmail.com>
2017-09-14 20:38:06 +0930

    Finish homework 27

--- chapter_27/hw27/main-race.c
+++ chapter_27/hw27/main-race.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "mythreads.h"
 
 int balance = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    Pthread_mutex_lock(&lock);
+    balance++; // unprotected access
+    Pthread_mutex_unlock(&lock);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
+
+    Pthread_mutex_lock(&lock);
     balance++; // unprotected access
+    Pthread_mutex_unlock(&lock);
+
     Pthread_join(p, NULL);
     return 0;
 }
